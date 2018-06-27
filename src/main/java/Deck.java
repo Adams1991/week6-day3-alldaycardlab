1cc656df15d5fc291122a09b04e6d5570999d9c9
Chris Craig <ctc2@live.co.uk>
2018-06-27 11:25:03 +0100

    deck set up with card count

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        this.deck = new ArrayList<>();
+    }
+
+
+    public int cardCount() {
+        return this.deck.size();
+    }
 }
--- src/test/java/TestDeck.java
+++ src/test/java/TestDeck.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestDeck {
+
+    Deck deck;
+    Card card;
+
+    @Before
+    public void setup(){
+        deck = new Deck();
+        card = new Card(SuitType.SPADES, RankType.EIGHT);
+    }
+
+    @Test
+    public void deckIsEmpty(){
+        assertEquals(0, deck.cardCount());
+    }
+
+
+
+
+
 }
