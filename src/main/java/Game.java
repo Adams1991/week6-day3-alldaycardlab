3b678e9c5d2fc2e1e6bb3ce572085a3bf594826e
Chris Craig <ctc2@live.co.uk>
2018-06-27 14:22:46 +0100

    can deal card method made

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,12 +3,18 @@ public class Game {
     private Deck deck;
     private Player player;
 
-    public Game(){
+    public Game(Deck deck, Player player){
+        this.deck = deck;
+        this.player = player;
 
     }
 
 
+    public void dealCard() {
+        deck.fillDeck();
+        Card card = this.deck.removeCard();
+        player.addCard(card);
 
-
+    }
 }
 
--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestGame {
+
+    Player player;
+    Deck deck;
+    Card card;
+    Game game;
+
+
+    @Before
+    public void setup(){
+        player = new Player("Boab");
+        deck = new Deck();
+        game = new Game(deck, player);
+    }
+
+    @Test
+    public void canDealCard(){
+        game.dealCard();
+        assertEquals(51, deck.cardCount());
+    }
+
+
 }
