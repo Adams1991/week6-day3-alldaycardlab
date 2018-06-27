5672008d9a34b0b498983cdaece8d62955315238
Chris Craig <ctc2@live.co.uk>
2018-06-27 13:32:40 +0100

    add card to hand method working

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Player {
+
+    private ArrayList<Card> hand;
+    private String name;
+
+    public Player(String name){
+        this.hand = new ArrayList<>();
+        this.name = name;
+    }
+
+
+    public int cardCount() {
+        return this.hand.size();
+    }
+
+    public void addCard(Card card) {
+        this.hand.add(card);
+    }
 }
--- src/test/java/TestPlayer.java
+++ src/test/java/TestPlayer.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPlayer {
+
+    Card card;
+    Deck deck;
+    Player player;
+
+    @Before
+    public void setup(){
+        deck = new Deck();
+        player = new Player("Chreun");
+    }
+
+    @Test
+    public void handIsEmpty(){
+        assertEquals(0, player.cardCount());
+    }
+
+    @Test
+    public void addCardToHand(){
+        deck.fillDeck();
+        card = deck.removeCard();
+        player.addCard(card);
+        assertEquals(1, player.cardCount());
+    }
+
+
+
+
+
+
 }
